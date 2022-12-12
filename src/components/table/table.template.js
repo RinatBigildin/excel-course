fe76f60edde9c4c754460ed8e4b5f373c603f579
RinatBigildin <88397838+RinatBigildin@users.noreply.github.com>
2022-12-12 13:20:23 +0400

    Resize (#4)
    * finish table resize
    
    * finish
    
    Co-authored-by: Rinat <rinatdeveloper1988@gmail.com>

--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -3,22 +3,31 @@ const CODES = {
   Z: 90,
 }
 
-function toCell(content) {
+function toCell(_, col) {
   return `
-    <div class="cell" contenteditable> ${content}</div>
+    <div class="cell" contenteditable data-col="${col}"></div>
     `;
 }
 
-function toColumn(col) {
+function toColumn(col, index) {
   return `
-    <div class="column">${col}</div>
+    <div class="column" data-type="resizable" data-col="${index}">
+    ${col}
+    <div class="col-resize" data-resize="col"></div>
+    </div>
     `;
 }
 
 function createRow(indexRow, content) {
+  const resize = indexRow
+  ? `<div class="row-resize" data-resize="row"></div>`
+  : '';
   return `
-    <div class="row">
-        <div class="row-info"> ${indexRow}</div>
+    <div class="row" data-type="resizable">
+        <div class="row-info">
+        ${indexRow ? indexRow: ''}
+        ${resize}
+        </div>
         <div class="row-data"> ${content} </div>
     </div>
     `;
@@ -31,14 +40,12 @@ function toChar(_, index) {
 export function createTable(rowsCount = 15) {
   const colsCount = CODES.Z - CODES.A + 1;
   const rows = [];
-  // eslint-disable-next-line max-len
   const cols = new Array(colsCount)
       .fill('')
       .map(toChar)
       .map(toColumn)
       .join('');
-  //   console.log(cols);
-  rows.push(createRow('', cols));
+  rows.push(createRow(null, cols));
   const cells = new Array(colsCount)
       .fill('')
       .map(toCell)
--- src/core/utils.js
+++ src/core/utils.js
@@ -5,3 +5,16 @@ export function capitalize(string) {
   }
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
+
+export function getCursorXY(objEvent) {
+  let x = null;
+  let y = null;
+  if (objEvent) {
+    x = objEvent.pageX;
+    y = objEvent.pageY;
+  } else {
+    x = window.event.clientX;
+    y = window.event.clientY;
+  }
+  return {x: x, y: y};
+}
